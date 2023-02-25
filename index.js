d22eb3bd64d0bb11f0a9dbc923d35e359cada174
Yanuar Fa <yanuarfatur41@gmail.com>
2023-02-25 21:45:12 +0700

    added express session, express cookies

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const expressLayouts = require("express-ejs-layouts");
+const session = require("express-session");
 const app = express();
+const LocalStorage = require("node-localstorage").LocalStorage,
+  localStorage = new LocalStorage("./scratch");
 
 const port = process.env.PORT || 5000;
 
 app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(
+  session({
+    secret: "awokwokwok apaan tuh",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(express.urlencoded({ extended: true }));
 
+function isAuthenticated(req, res, next) {
+  if (req.session.user) next();
+  else next("route");
+}
+
 app.get("/", (req, res) => {
-  res.redirect("/dashboard");
+  res.redirect("/login");
 });
 
-app.get("/dashboard", (req, res) => {
+app.get("/dashboard", isAuthenticated, (req, res) => {
   res.render("index", {
     layout: "partials/main-layout",
     title: "Dashboard",
@@ -59,6 +74,10 @@ app.get("/login", (req, res) => {
     layout: "login",
     title: "Login",
   });
+});
+
+app.post("/login", (req, res) => {
+  res.json(req.body);
 });
 
 app.listen(port, () => {
